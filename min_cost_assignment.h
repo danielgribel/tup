22d9c836ab979909515681577a5339cdf5e8860e
Daniel Lemes Gribel <gribel.daniel@gmail.com>
2016-06-27 20:45:01 -0300

    renaming MinAssignment and Main

--- min_cost_assignment.h
+++
@@ -1,9 +0,0 @@
-#ifndef Min_Cost_Assignment
-#define Min_Cost_Assignment
-
-#include "dlib-master/dlib/optimization/max_cost_assignment.h"
-#include <iostream>
-
-std::vector<long> minAssignment(double** mat, int m);
-
-#endif
